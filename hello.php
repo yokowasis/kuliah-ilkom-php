dd55cff98499475787e62d29dce0536d08b980fd
Wasis Galaxy TAB S8 <yokowasis@gmail.com>
2022-10-13 20:43:22 +0800

    test emmet php lynx terminal browser

--- hello.php
+++ hello.php
@@ -1,8 +1,8 @@
 <?php
 // Hello World
-echo "Hello World";
-
-echo "Hello Mantap <br/>";
-for ($i = 0; $i <= 10 ; $i++) {
-    echo "Hello $i <br/>";
+echo "<p>Hello Mantap</p>";
+echo "<h1>Heading</h1>";
+echo "<h2>Heading</h2>";
+for ($i = 0; $i <= 3 ; $i++) {
+    echo "<p>Hello <a href='https://google.com/'>$i</a></p>";
 }
---
+++ phpmon.sh
@@ -0,0 +1,1 @@
+when-changed $1 -c "clear;php $1 | lynx -stdin --dump"
