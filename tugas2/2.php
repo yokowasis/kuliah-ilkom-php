d5f81eeaab50442142ecdc22a03e975d9a3a5eb9
Yokowasis - GreenCloud <yokowasis@gmail.com>
2022-11-27 17:00:01 +0800

    Tugas 2 OK

---
+++ tugas2/2.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title></title>
+</head>
+<body>
+<?php
+//various date format in php
+echo date("d/m/Y") . "<br>";
+echo date("d.m.Y") . "<br>";
+echo date("d-m-Y") . "<br>";
+echo date("l") . "<br>";
+echo date("D") . "<br>";
+echo date("d") . "<br>";
+echo date("m") . "<br>";
+echo date("Y") . "<br>";
+echo date("H") . "<br>";
+echo date("i") . "<br>";
+echo date("s") . "<br>";
+echo date("a") . "<br>";
+echo date("A") . "<br>";
+echo date("h:i:sa") . "<br>";
+?>
+</body>
+</html>
---
+++ tugas2/3.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title></title>
+</head>
+<body>
+  <h2>Biodata</h2>
+  <p>Nama : <?= "Wasis Haryo Sasoko"?></p>
+  <p>TTL : <?= "Ampenan, 8 November 1986"?></p>
+  <p>ALamat : <?= "Rabangodu Utara"?></p>
+
+</body>
+</html>
