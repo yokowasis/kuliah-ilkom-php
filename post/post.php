c6496cb8a2b11040341ab00a382266eaf5a17e8f
Yokowasis S22 <yokowasis@gmail.com>
2022-12-03 14:26:10 +0800

    post

---
+++ post/post.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title></title>
+</head>
+<body>
+  <form action="postAct.php" method="POST">
+    <table>
+      <tr>
+        <td>Nama</td>
+        <td><input type="text" name="nama"/></td>
+      </tr>
+      <tr>
+        <td>Email</td>
+        <td><input type="text" name="email"/></td>
+      </tr>
+      <tr>
+        <td colspan="2">
+          <input type="submit" value="Login"/>
+          <input type="reset" />
+        </td>
+      </tr>
+    </table>
+  </form>
+</body>
+</html>
---
+++ post/postAct.php
@@ -0,0 +1,5 @@
+<?php
+echo "<div align='center'>";
+echo "Nama : {$_POST["nama"]} <br/> ";
+echo "Email : {$_POST["email"]} <br/> ";
+echo "</div>";
