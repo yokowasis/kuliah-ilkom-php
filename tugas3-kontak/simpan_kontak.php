b76f6dfb4b7d18f8d8f01e9e1912744105b8c0ac
Wasis - Ubuntu Tab <yokowasis@gmail.com>
2022-12-17 12:13:22 +0800

    tugas

--- tugas3-kontak/simpan_kontak.php
+++ tugas3-kontak/simpan_kontak.php
@@ -13,14 +13,14 @@ if ($conn->connect_error) {
 
 print_r($_POST);
 
-// $sql = "INSERT INTO kontak (nama, jkel, email, alamat, kota, pesan) VALUES ('" . $_POST['nama'] . "', '" . $_POST['jkel'] . "', '" . $_POST['email'] . "', '" . $_POST['alamat'] . "', '" . $_POST['kota'] . "', '" . $_POST['pesan'] . "')";
+$sql = "INSERT INTO kontak (nama, jkel, email, alamat, kota, pesan) VALUES ('" . $_POST['nama'] . "', '" . $_POST['jkel'] . "', '" . $_POST['email'] . "', '" . $_POST['alamat'] . "', '" . $_POST['kota'] . "', '" . $_POST['pesan'] . "')";
 
-// echo $sql;
+echo $sql;
 
-// if (
-//   $conn->query($sql) === TRUE
-// ) {
-//   echo "New record created successfully";
-// } else {
-//   echo "Error: " . $sql . "<br>" . $conn->error;
-// }
+if (
+  $conn->query($sql) === TRUE
+) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
